d85bfbd56a356f93ea73dd3108e92c356029ffb8
Родион Сулохин <sulohin007@mail.ru>
2020-05-27 20:05:43 +0300

    4

---
+++ Restorans/OpenData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+public class OpenData
+{
+    private static FoodPlace[] restorans;
+
+	public OpenData(string fileName)
+	{
+        string fullPath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+	}
+
+    public class PublicPhoneItem
+    {
+        public string PublicPhone { get; set; }
+    }
+
+    public class GeoData
+    {
+        public string type { get; set; }
+        public List<double> coordinates { get; set; }
+    }
+
+    public class FoodPlace
+    {
+        public int global_id { get; set; }
+
+        public string Latitude_WGS84 { get; set; }
+
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string IsNetObject { get; set; }
+
+        public string TypeObject { get; set; }
+
+        public string AdmArea { get; set; }
+
+        public string District { get; set; }
+
+        public string Address { get; set; }
+
+        public List<PublicPhoneItem> PublicPhone { get; set; }
+
+        public int SeatsCount { get; set; }
+
+        public string SocialPrivileges { get; set; }
+
+        public string Longitude_WGS84 { get; set; }
+
+        public GeoData geoData { get; set; }
+    }
+
+
+}
