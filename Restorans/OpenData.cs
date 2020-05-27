4bda76b2aa40e3a91878ec1f46e9d48e74a062c4
Родион Сулохин <sulohin007@mail.ru>
2020-05-28 00:56:45 +0300

    Сделал частичную десериализацию

--- Restorans/OpenData.cs
+++ Restorans/OpenData.cs
@@ -72,29 +72,14 @@ public class PublicPhoneItem
     public string PublicPhone { get; set; }
 }
 
-public class GeoData
-{
-    public string type { get; set; }
-    public List<double> coordinates { get; set; }
-}
-
 public class FoodPlace
 {
-    public int global_id { get; set; }
-
-    public string Latitude_WGS84 { get; set; }
-
-    public string ID { get; set; }
 
     public string Name { get; set; }
 
     public string IsNetObject { get; set; }
 
     public string TypeObject { get; set; }
-
-    public string AdmArea { get; set; }
-
-    public string District { get; set; }
 
     public string Address { get; set; }
 
@@ -103,8 +88,4 @@ public class FoodPlace
     public int SeatsCount { get; set; }
 
     public string SocialPrivileges { get; set; }
-
-    public string Longitude_WGS84 { get; set; }
-
-    public GeoData geoData { get; set; }
 }
